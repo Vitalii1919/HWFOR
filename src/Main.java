85e318cc4ef8e6c76cafd500df7af71f806647d6
origin <jerz20@mail.ru>
2025-11-28 20:36:45 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,44 @@
 public class Main {
     public static void main(String[] args) {
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        for (int year = 1904; year <= 2096; year = year + 4) {
+            System.out.println("год явлляется вискокосным" + year);
+        }
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
+        for (int i = 1; i < 1024; i = i * 2) {
+            System.out.println(i);
+        }
+        int cash = 29000;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + cash;
+            System.out.println("Месяц" + i + "сумма накоплений равна" + total + "рублей");
+        }
+        int cash1 = 29000;
+        int total1 = 0;
+        for (int i = 0; i < 12; i++) {
+            total1 = total1 + total1 / 100;
+            total1 = total1 + cash1;
+            System.out.println("Месяц" + i + "Итого" + total1);
+        }
+        System.out.println(total1);
+        for (int o = 1; o <= 10; o++) {
+            int result = 2 * o;
+            System.out.println("2 * " + o + " = " + result);
         }
     }
 }
